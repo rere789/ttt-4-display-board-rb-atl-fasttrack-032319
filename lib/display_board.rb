93c790d87caa10e3ba54c047d18a842bd93268e1
Reina Ewing <reina.castro3@gmail.com>
2019-03-24 14:22:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  lines = "----------"
   
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts " #{lines} "
+  puts "----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts " #{lines} "
+  puts "----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
